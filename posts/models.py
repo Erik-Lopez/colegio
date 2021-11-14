4154d5f127ebcf99d2a2cf40d9e8aa04abc326f7
Erik <eriklf442@gmail.com>
2021-11-14 15:04:49 +0100

    Add basic post app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
+# Python
+from enum import Enum
+
+# Django
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    pass
+
+class Club(models.Model):
+    pass
+
+class Tag(Enum):
+    TAG1 = 1
+    TAG2 = auto()
+
+class BlogPost(models.Model):
+#    post_id
+
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    author_id = models.ForeignKey(User, on_delete=models.SET_NULL)
+#    tags_id   = models.ForeignKey(Tag, on_delete=models.SET_NULL)
+    club_id   = models.ForeignKey(Club, on_delete=models.SET_NULL)
+
+    posted_at = models.Date
